8a962be98029f5a7d8f19239ca8dc549d2cfcb33
Phil Gibbins <scoobster17@hotmail.co.uk>
2019-04-09 14:23:55 +0100

    Add NVM (and GC) to fish local; in bash local but not fish local

--- .config/fish/local.fish
+++ .config/fish/local.fish
@@ -4,3 +4,10 @@ set_color normal;
 
 set -x PROJECTS_DIR "~/Documents/path/to/projects"
 echo "Projects directory set to" $PROJECTS_DIR;
+
+# Google Cloud
+# bass source $HOME/Documents/tools/google-cloud-sdk/path.bash.inc
+
+# NVM
+# export NVM_DIR=~/.nvm
+# source $(brew --prefix nvm)/nvm.sh
