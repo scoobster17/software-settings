1e9b74ef1d7f3472875e840a88b6bbe46e14401a
Phil Gibbins <scoobster17@hotmail.co.uk>
2019-12-13 12:25:08 +0000

    Changes

--- setup/mac/index.bash
+++ setup/mac/index.bash
@@ -1,11 +1,11 @@
 # Install brew to download all apps and other tools
-source ./tools/install-brew.bash
+source $(cd tools; pwd)/install-brew.bash
 
 # Set certain machine configurations before installing apps/tools
 source ./machine-configuration.bash
 
 # Install apps
-source ./apps/install-casks.bash
+source $(cd apps; pwd)/install-casks.bash
 
 # Set up apps
-source ./apps/set-up-google-chrome.bash
+source $(cd apps; pwd)/set-up-google-chrome.bash
--- setup/mac/tools/install-brew.bash
+++ setup/mac/tools/install-brew.bash
@@ -6,5 +6,5 @@ xcode-select --install
 # Install Homebrew for packages e.g. ffmpeg, fish, git, nginx, nvm, trash, zsh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Homebrew Cask for Mac OS apps, e.g. Google Chrome, ITerm2
-brew install caskroom/cask/brew-cask
+# No need to install Homebrew Cask any more; done automatically by brew.
+# Casks are used for Mac OS apps, e.g. Google Chrome, ITerm2
