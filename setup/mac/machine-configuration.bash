e386e07cca41a5f2e30efcab81b65a2c66593484
Phil Gibbins <scoobster17@hotmail.co.uk>
2019-12-13 12:58:21 +0000

    Fix to keep repo doing all the work :facepalm:

--- setup/mac/machine-configuration.bash
+++ setup/mac/machine-configuration.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+CURRENT_DOWNLOADS_PATH=~/Downloads
+NEW_DOWNLOADS_PATH=~/Desktop
+
 # Make all downloads go to Desktop
-# Doing it this way means each app doesn't need to be set up individually
-sudo rm -rf ~/Downloads
-ln -s ~/Desktop ~/Downloads
+# Deleting and creating a symlink means each app doesn't need to be set up individually
+mv $CURRENT_DOWNLOADS_PATH"/*" $NEW_DOWNLOADS_PATH
+sudo rm -rf $CURRENT_DOWNLOADS_PATH
+ln -s $NEW_DOWNLOADS_PATH $CURRENT_DOWNLOADS_PATH
