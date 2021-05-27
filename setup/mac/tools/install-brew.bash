fb9717024d809885a572ee0a3fc23c3643b6d668
Phil Gibbins <philg@Phils-MP.local>
2021-05-27 21:48:56 +0100

    brew install migration from ruby to bash

--- setup/mac/tools/install-brew.bash
+++ setup/mac/tools/install-brew.bash
@@ -4,7 +4,7 @@
 xcode-select --install
 
 # Install Homebrew for packages e.g. ffmpeg, fish, git, nginx, nvm, trash, zsh
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # No need to install Homebrew Cask any more; done automatically by brew.
 # Casks are used for Mac OS apps, e.g. Google Chrome, ITerm2
