ff84af93e74f2fb301ac480a5d44d5d8da2d3884
Phil Gibbins <scoobster17@hotmail.co.uk>
2022-02-18 23:55:13 +0000

    Remove unnecessary sudo, configure ssh config

--- setup/mac/fetch-configurations/fetch-settings.sh
+++ setup/mac/fetch-configurations/fetch-settings.sh
@@ -8,3 +8,7 @@ cd "$PROJECTS_DIR/personal"
 echo "Navigated to $PROJECTS_DIR/personal"
 
 gh repo clone scoobster17/software-settings
+
+# auth cleanup
+trash ~/.ssh/config
+ln -s $PROJECTS_DIR/personal/software-settings/.ssh/config ~/.ssh/config
--- setup/mac/machine-configuration/configure-sublime-text.sh
+++ setup/mac/machine-configuration/configure-sublime-text.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p ~/bin
-sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
