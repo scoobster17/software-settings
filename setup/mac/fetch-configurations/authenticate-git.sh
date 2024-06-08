8f34f9c6235837c68bb5b719afc15079e94dc222
Phil Gibbins <scoobster17@hotmail.co.uk>
2024-06-08 20:53:44 +0100

    Fix github script

--- setup/mac/fetch-configurations/authenticate-git.sh
+++ setup/mac/fetch-configurations/authenticate-git.sh
@@ -8,18 +8,18 @@ NOCOLOR="\033[0m"
 RED="\033[0;31m"
 YELLOW="\033[1;33m"
 
-if [ ! -f ~/.ssh/github_ed25519 ]; then
+if [ ! -f ~/.ssh/github_ed25519_personal ]; then
   if [ ! -f ~/.ssh/id_ed25519 ]; then
     echo -e "${RED}Please reauthenticate gh and create an ssh key${NOCOLOR}"
   else
-  	read -p "Do you want to rename id_ed25519(.pub) to github_ed25519(.pub)? (Y/n) " -n 1 answer
+  	read -p "Do you want to rename id_ed25519(.pub) to github_ed25519_personal(.pub)? (Y/n) " -n 1 answer
   	echo
   	if [[ "$answer" == $'\0A' || "$answer" =~ ^[Yy]$ ]]; then
-    	echo "Renaming id_ed25519 to github_ed25519..."
-    	mv ~/.ssh/id_ed25519 ~/.ssh/github_ed25519
-    	echo "Renaming id_ed25519.pub to github_ed25519.pub..."
-    	mv ~/.ssh/id_ed25519.pub ~/.ssh/github_ed25519.pub
-    	echo "Renamed id_ed25519(.pub) to github_ed25519(.pub)"
+    	echo "Renaming id_ed25519 to github_ed25519_personal..."
+    	mv ~/.ssh/id_ed25519 ~/.ssh/github_ed25519_personal
+    	echo "Renaming id_ed25519.pub to github_ed25519_personal.pub..."
+    	mv ~/.ssh/id_ed25519.pub ~/.ssh/github_ed25519_personal.pub
+    	echo "Renamed id_ed25519(.pub) to github_ed25519_personal(.pub)"
     else
       echo -e "${YELLOW}Make sure to remember what id_ed25519(.pub) is used for!${NOCOLOR}"
   	fi
@@ -28,4 +28,4 @@ fi
 
 touch ~/.ssh/config
 chmod 600 ~/.ssh/config
-echo -e "Host github.com\n  IdentityFile ~/.ssh/github_ed25519" >> ~/.ssh/config
+echo -e "Host github.com\n  IdentityFile ~/.ssh/github_ed25519_personal" >> ~/.ssh/config
