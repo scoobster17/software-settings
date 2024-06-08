aafdf2f4582ac9d623dcaf1168894357493a3ed8
Phil Gibbins <scoobster17@hotmail.co.uk>
2024-06-08 23:14:40 +0100

    Add work repos question

--- os-specific/mac/default-setup/fetch-configurations/create-repos-directories.sh
+++ os-specific/mac/default-setup/fetch-configurations/create-repos-directories.sh
@@ -12,8 +12,14 @@ else
 fi
 
 if [ ! -d $WORK_REPOS_DIR ]; then
-  mkdir -p $WORK_REPOS_DIR
-  echo "Created work repos folder"
+  read -p "Will you install work repos on this machine? (Y/n) " -n 1 answer
+  echo
+  if [[ "$answer" == $'\0A' || "$answer" =~ ^[Yy]$ ]]; then
+    mkdir -p $WORK_REPOS_DIR
+    touch WORK_REPOS_DIR"/.gitconfig"
+    open -g WORK_REPOS_DIR"/.gitconfig"
+    echo "Created work repos folder. !!! Remember to enter work email as git username in work repos dir !!!"
+  fi
 else
   echo $WORK_REPOS_DIR" already exists"
 fi
