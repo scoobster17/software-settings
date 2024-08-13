b4405c6ab41188deffa0f786415f2cf843ab5913
Phil Gibbins <scoobster17@hotmail.co.uk>
2024-08-13 23:21:27 +0100

    Add automation to move downloads to desktop hourly

--- os-specific/mac/default-user-setup/machine-configuration/configure-login-items.sh
+++ os-specific/mac/default-user-setup/machine-configuration/configure-login-items.sh
@@ -4,5 +4,6 @@ DIR=$( dirname "$BASH_SOURCE" | sed "s:/./:/:" )
 
 source "$DIR/conditional_add_login_item.sh"
 
-conditional_add_login_item CopyClip
-conditional_add_login_item Spectacle
+# conditional_add_login_item CopyClip
+# conditional_add_login_item Spectacle
+conditional_add_login_item $PERSONAL_REPOS_DIR"/software-settings/os-specific/mac/default-user-setup/automator/Automation | Move Downloads to Desktop Hourly.app"
